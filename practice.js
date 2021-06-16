af3c73e51cf3a7f17321d141755496b3ab091d7e
Andy G <52140974+amgauthier1992@users.noreply.github.com>
2021-06-16 12:42:11 -0400

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,6 +1,6 @@
-Given an array of integers, find the sum of its elements.
+//Given an array of integers, find the sum of its elements.
 
-For example, if the array [1,2,3], 1 + 2 + 3, so return 6.
+//For example, if the array [1,2,3], 1 + 2 + 3, so return 6.
 
 function simpleArraySum(ar) {
   let sum = 0;
@@ -10,7 +10,7 @@ function simpleArraySum(ar) {
   return sum;
 }
 
-In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+//In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
 
 function aVeryBigSum(ar) {
   let bigSum = 0
@@ -20,18 +20,20 @@ function aVeryBigSum(ar) {
   return BigInt(bigSum);
 }
 
-Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
+/* 
+   Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
 
-The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
+   The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
 
-The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
+   The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
 
-If a[i] > b[i], then Alice is awarded 1 point.
-If a[i] < b[i], then Bob is awarded 1 point.
-If a[i] = b[i], then neither person receives a point.
-Comparison points is the total points a person earned.
+   If a[i] > b[i], then Alice is awarded 1 point.
+   If a[i] < b[i], then Bob is awarded 1 point.
+   If a[i] = b[i], then neither person receives a point.
+   Comparison points is the total points a person earned.
 
-Given a and b, determine their respective comparison points.
+   Given a and b, determine their respective comparison points.
+*/
 
 function compareTriplets(a, b) {
   //return integer arr as result
@@ -51,6 +53,7 @@ function compareTriplets(a, b) {
   return result;
 }
 
+/*
 Given a square matrix, calculate the absolute difference between the sums of its diagonals.
 
 For example, the square matrix arr is shown below:
@@ -59,6 +62,7 @@ For example, the square matrix arr is shown below:
 4 5 6
 9 8 9  
 The left-to-right diagonal = 15. The right to left diagonal = 17. Their absolute difference is 2. |15-17| = 2
+*/
 
 function diagonalDifference(arr) {
   let d1 = 0;
